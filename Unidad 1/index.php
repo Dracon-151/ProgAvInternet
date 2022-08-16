56e07ae0ba0d972fec049ae18c7808c325609fb4
Dracon-151 <demianalexisalvarado@gmail.com>
2022-08-16 10:54:14 -0600

    Canvas clase 2 - eventos

--- Unidad 1/index.php
+++ Unidad 1/index.php
@@ -4,17 +4,18 @@
         <title>Canvas</title>
     </head>
     <body>
-        <canvas width="1000" height="720" id="cvs">
+        <canvas width="800" height="720" id="cvs">
           Hola tu navegador no funciona
         </canvas>
 
+        <img id="imagen" src="img/boombot.png" style="display: none">
         <script>
           var canvas = document.getElementById('cvs');
           var ctx = canvas.getContext('2d');
 
-          ctx.fillStyle = "rgb(138,138,209)";
+          /*ctx.fillStyle = "rgb(138,138,209)";
           ctx.fillRect(50,50,60,60);
-
+          
           ctx.fillStyle = "rgba(209,138,138, 0.9)";
           ctx.fillRect(90,90,60,60);
 
@@ -46,6 +47,55 @@
           ctx.beginPath();
           ctx.arc(600,100, 50, 0, 2 * Math.PI);
           ctx.stroke();
+          */
+
+          ctx.font = "80px Arial";
+          ctx.fillStyle = "rgb(255,100,100)";
+          ctx.fillText("Hola mundo", 100, 100);
+          ctx.strokeText("Hola mundo", 100, 200);
+          
+          
+          var grd = ctx.createLinearGradient(500,300,700,380);
+          grd.addColorStop(0,  "orange")
+          grd.addColorStop(1,  "purple")
+          ctx.fillStyle = grd;
+          ctx.fillRect(500,300,200,80);
+          
+          
+          var grd = ctx.createRadialGradient(600,380,5,600,380,150);
+          grd.addColorStop(0,  "red")
+          grd.addColorStop(1,  "white")
+          ctx.fillStyle = grd;
+          ctx.fillRect(500,300,200,160);
+
+          var img = document.getElementById("imagen");
+          ctx.drawImage(img, 200, 200, 150, 150);
+
+          canvas.addEventListener("click", function (e){              
+            ctx.strokeStyle = "rgb(100, 100, 100)";
+            ctx.fillStyle = "rgba(138,138,209, 0.8)";
+            ctx.beginPath();
+            ctx.arc(e.offsetX,e.offsetY , 25, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+          });
+          
+          canvas.addEventListener("mouseover", function (e){
+
+            var r = Math.random()*255;
+            var g = Math.random()*255;
+            var b = Math.random()*255;
+                        
+            ctx.strokeStyle = "rgb(100, 100, 100)";
+            ctx.fillStyle = "rgba(" + r + "," + g + "," + b + ", 0.8)";
+          });
+
+          canvas.addEventListener("click", function (e){
+            ctx.beginPath();
+            ctx.arc(e.offsetX,e.offsetY , 25, 0, 2 * Math.PI);
+            ctx.stroke();
+            ctx.fill();
+          });
 
         </script>
     </body>
