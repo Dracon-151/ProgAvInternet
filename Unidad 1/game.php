9eccf01a538f1f1128138a0e7414d2d959caff4a
Dracon-151 <demianalexisalvarado@gmail.com>
2022-08-25 09:21:40 -0600

    23.- Utilizando requestAnimationFrame y otras funciones crear obstáculos e implementar pausa

--- Unidad 1/game.php
+++ Unidad 1/game.php
@@ -13,17 +13,23 @@
         var ctx = null;
         var player1 = null;
         var player2 = null;
+        var paredes = null;
         var direccion = 1;
         var puntos = 0;
         var speed = 5;
+        var pause = false;
 
         function run()
         {
             canvas = document.getElementById('cvs');
             ctx = canvas.getContext('2d');
 
-            player1 = new Cuadro(50,50,50,50,"rgb(138,209,138");
+            player1 = new Cuadro(200,200,50,50,"rgb(138,209,138");
             player2 = new Cuadro(300,300,25,25,"rgb(209,138,138");
+            paredes = [new Cuadro(80,175,25,250,"rgb(105,100,100"), 
+                    new Cuadro(720,175,25,250,"rgb(1050100,100"), 
+                    new Cuadro(110,80,600,25,"rgb(100,100,100"), 
+                    new Cuadro(110,500,600,25,"rgb(100,100,100")];
             paint();
         }
 
@@ -41,17 +47,22 @@
         document.addEventListener("keydown", function(e){
             console.log(e.keyCode);
 
-            if(e.keyCode == 37 || e.keyCode == 65){
-                direccion = 2;
+            if(!pause){
+                if(e.keyCode == 37 || e.keyCode == 65){
+                    direccion = 2;
+                }
+                if(e.keyCode == 38 || e.keyCode == 87){
+                    direccion = 4;
+                }
+                if(e.keyCode == 39 || e.keyCode == 68){
+                    direccion = 1;
+                }
+                if(e.keyCode == 40 || e.keyCode == 83){
+                    direccion = 3;
+                }
             }
-            if(e.keyCode == 38 || e.keyCode == 87){
-                direccion = 4;
-            }
-            if(e.keyCode == 39 || e.keyCode == 68){
-                direccion = 1;
-            }
-            if(e.keyCode == 40 || e.keyCode == 83){
-                direccion = 3;
+            if(e.keyCode == 32){
+                pause = !pause;
             }
         });
 
@@ -62,8 +73,6 @@
             window.requestAnimationFrame(paint);
             ctx.fillStyle = "rgb(255,255,255)";
             ctx.fillRect(0,0,800,600);
-            ctx.fill();
-            ctx.stroke();
             
             ctx.font = "20px Arial";
             ctx.fillStyle = "rgb(100,100,100)";
@@ -71,15 +80,49 @@
 
             bounds();
 
-            move();
+            if(!pause){
+                move();
+            }
+
+            for (var i = 0; i < paredes.length; i++) {
+                paredes[i].paint(ctx);
+                if(player1.se_tocan(paredes[i])){
+                    switch(direccion){
+                        case 1:
+                            player1.x -= speed;
+                        break;
+                        case 2:
+                            player1.x += speed;
+                        break;
+                        case 3:
+                            player1.y -= speed;
+                        break;
+                        case 4:
+                            player1.y += speed;
+                        break;
+                    }
+                }
+            }
 
             player1.paint(ctx);
 
             if(player1.se_tocan(player2)){
-                player2.x = Math.random()* 680;
-                player2.y = Math.random()* 600;
+                player2.x = Math.random()* 500 + 100;
+                player2.y = Math.random()* 200 + 200;
+                player1.c = "rgb(" + Math.random()*255 + "," + Math.random()*255 + "," + Math.random()*255 + ")";
+                    puntos += 10;
+                    speed += 0.5;
             }
             player2.paint(ctx);
+
+            if(pause){
+                ctx.fillStyle = "rgba(0,0,0,0.4)";
+                ctx.fillRect(0,0,800,600);
+                
+                ctx.font = "20px Arial";
+                ctx.fillStyle = "rgb(255,255,255)";
+                ctx.fillText("Pausa", 350, 320);
+            }
         }
 
         function bounds(){
@@ -129,9 +172,6 @@
                 this.x + this.w > target.x && 
                 this.y < target.y + target.h && 
                 this.y + this.h > target.y){
-                    this.c = "rgb(" + Math.random()*255 + "," + Math.random()*255 + "," + Math.random()*255 + ")";
-                    puntos += 10;
-                    speed += 0.5;
                     return true;
                 }
             };
