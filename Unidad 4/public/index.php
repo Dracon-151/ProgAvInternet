c2f581b89f9aa3e906ec61c07a96ecf01b06568b
Dracon-151 <demianalexisalvarado@gmail.com>
2022-09-15 09:57:25 -0600

    Less

---
+++ Unidad 4/public/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <link rel="stylesheet" type="text/css" href="css/main.css">
+        <title>Index</title>
+    </head>
+    <body>
+        <div class="container">
+            <fieldset>
+                <legend>Login</legend>
+                <form>
+                    <label for="email">Email</label>
+                    <input type="email" id="email">
+
+                    <label for="password">Password</label>
+                    <input type="password" id="password">
+
+                    <button type="submit">
+                        Log in
+                    </button>
+                </form>
+            </fieldset>
+        </div>
+    </body>
+</html>
