b41e04e8598a99557fcd220244263e70f8625675
Dracon-151 <demianalexisalvarado@gmail.com>
2022-11-08 09:48:21 -0700

    Implementa Laravel Fortify para poder hacer login con un usuario

--- Unidad5/progAvInternetLaravel/routes/web.php
+++ Unidad5/progAvInternetLaravel/routes/web.php
@@ -22,6 +22,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/login', function () {
+    return view('login');
+})->name('login');
+
 Route::get('saludo', function () {
     echo "Hola";
 });
